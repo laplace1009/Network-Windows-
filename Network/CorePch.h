13ede2b6fe5a2e80ede93a312f821ab24b003f7f
부익 김 <laplace1009@gmail.com>
2024-02-06 00:51:23 +0900

    공통 유틸 정의

---
+++ Network/CorePch.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Types.h"
+#include "Macro.h"
+
+#include <iostream>
+#include <string_view>
+
+#include <Windows.h>
+#include <WinSock2.h>
+#include <MSWSock.h>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
---
+++ Network/Macro.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Types.h"
+
+#define CRASH(cause)					\
+{										\
+	uint32* crash = nullptr;				\
+	__analysis_assume(crash != nullptr);\
+	*crash = 0xDEADBEEF;				\
+}
+
+#define ASSERT_CRASH(expr)\
+{								\
+	if ((expr == false))		\
+	{							\
+		CRASH("ASSERT_CRASH")	\
+		__analysis_assume(expr);\
+	}							\
+}
