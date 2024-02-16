4a90374a83b717d19545d190b8000893ded8e986
부익 김 <laplace1009@gmail.com>
2024-02-16 16:36:00 +0900

    Select class start 함수 수정 및 Subscribes 함수 추가

--- Network/Select.cpp
+++ Network/Select.cpp
@@ -2,10 +2,8 @@
 #include "Select.h"
 
 Select::Select()
-	:mStreams(FD_SETSIZE)
 {
-	FD_ZERO(&mReads);
-	FD_ZERO(&mWrites);
+	mStreams.reserve(FD_SETSIZE);
 }
 
 auto Select::GetSocketSize() -> uint64
@@ -13,11 +11,25 @@ auto Select::GetSocketSize() -> uint64
 	return mStreams.size();
 }
 
-auto Select::Start(timeval time) -> void
+auto Select::Start(TcpListener& server, timeval time) -> int
 {
+	FD_ZERO(&mReads);
+	FD_ZERO(&mWrites);
+	FD_SET(server.GetServerSocketPtr()->GetSocketInfoPtr()->socket, &mReads);
 	for (TcpStream& stream : mStreams)
 	{
 		FD_SET(stream.GetSocketInfoPtr()->socket, &mReads);
 		FD_SET(stream.GetSocketInfoPtr()->socket, &mWrites);
 	}
+	return select(0, &mReads, &mWrites, NULL, NULL);
+}
+
+auto Select::SubScribes(TcpStream stream) -> void
+{
+	mStreams.emplace_back(std::move(stream));
+}
+
+auto Select::SubScribes(std::initializer_list<TcpStream> list) -> void
+{
+	mStreams.insert(mStreams.end(), std::make_move_iterator(list.begin()), std::make_move_iterator(list.end()));
 }
--- Network/Select.h
+++ Network/Select.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Types.h"
 #include "TcpStream.h"
+#include "TcpListener.h"
 #include "mutex"
 
 
@@ -11,11 +12,12 @@ public:
 
 public:
 	auto GetSocketSize() -> uint64;
-	auto Start(timeval time) -> void;
+	auto Start(TcpListener& server, timeval time) -> int;
+	auto SubScribes(TcpStream stream) -> void;
+	auto SubScribes(std::initializer_list<TcpStream> list) -> void;
 
 private:
 	std::vector<TcpStream> mStreams;
 	fd_set mReads;
 	fd_set mWrites;
-	std::mutex mLock;
 };
