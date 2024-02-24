0413f33867000f1ad0f8af92d5fd331186e4986d
부익 김 <laplace1009@gmail.com>
2024-02-25 00:07:20 +0900

    TcpStream class INADDR_ANY, ADDR 로 나누기

--- Network/TcpListener.cpp
+++ Network/TcpListener.cpp
@@ -1,7 +1,7 @@
 #include "pch.h"
 #include "TcpListener.h"
 
-auto TcpListener::Bind(std::string_view addr, uint16 port) -> void
+auto TcpListener::BindAny(uint16 port) -> void
 {
 	mServer.GetSocketInfoPtr()->addr.sin_family = AF_INET;
 	mServer.GetSocketInfoPtr()->addr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -9,6 +9,27 @@ auto TcpListener::Bind(std::string_view addr, uint16 port) -> void
 	if (SOCKET_ERROR == bind(mServer.GetSocketInfoPtr()->socket, reinterpret_cast<SOCKADDR*>(&mServer.GetSocketInfoPtr()->addr), sizeof(mServer.GetSocketInfoPtr()->addr)))
 		return;
 
+	if (SOCKET_ERROR == listen(mServer.GetSocketInfoPtr()->socket, SOMAXCONN))
+		return;
+}
+
+auto TcpListener::Bind(std::string_view addr, uint16 port) -> void
+{
+	mServer.GetSocketInfoPtr()->addr.sin_family = AF_INET;
+	mServer.GetSocketInfoPtr()->addr.sin_port = htons(port);
+	int isSuccess = inet_pton(AF_INET, addr.data(), &mServer.GetSocketInfoPtr()->addr);
+	// wrong string address
+	if (0 == isSuccess)
+	{
+		return;
+	}
+	// internal error
+	if (-1 == isSuccess)
+	{
+		return;
+	}
+	if (SOCKET_ERROR == bind(mServer.GetSocketInfoPtr()->socket, reinterpret_cast<SOCKADDR*>(&mServer.GetSocketInfoPtr()->addr), sizeof(mServer.GetSocketInfoPtr()->addr)))
+		return;
 	if (SOCKET_ERROR == listen(mServer.GetSocketInfoPtr()->socket, SOMAXCONN))
 		return;
 }
--- Network/TcpListener.h
+++ Network/TcpListener.h
@@ -6,6 +6,7 @@
 class TcpListener
 {
 public:
+	auto BindAny(uint16 port) -> void;
 	auto Bind(std::string_view addr, uint16 port) -> void;
 	auto Accept() -> std::optional<TcpStream>;
 	auto Recv(OUT TcpStream* client) -> int;
